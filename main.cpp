874e275674c573a6788dd083bcafb7ab926c74f9
Batawi <batawi@debian>
2019-02-19 16:37:12 +0100

    big update of everything

--- main.cpp
+++ main.cpp
@@ -1,85 +1,17 @@
-#include <SFML/Graphics.hpp>
-#include <iostream>
-using namespace std;
+#include "mandelbrot.h"
 
-#define WINDOW_WIDTH 500
+#define WINDOW_WIDTH 700
 #define WINDOW_HEIGHT 500
 
-void Mandelbrot(sf::VertexArray& points_arr)
-{
-    //recursive formula for mandelbrot set, c belongs to Imaginary set
-    //Z0 = 0
-    //Zn+1 = (Zn)^2 + c
-    //c = a + bi
-    //sqrt(-1) = i
-    
-    double coord_real; //real component coordinate
-    double coord_imaginary; //imaginary component coordinate
-
-    double prev_real;
-    double prev_imaginary;
-
-    double next_real;
-    double next_imaginary;
-
-    //slopes for later values mapping
-    double slope_a = 1.0 * (2-(-2)) / (WINDOW_HEIGHT-0);
-    double slope_b = 1.0 * (2-(-2)) / (WINDOW_WIDTH-0);
-
-    unsigned int max_iterations = 100;
-    unsigned int counter = 0;
-
-    sf::Color color1 = sf::Color::Black;
-    sf::Color color2 = sf::Color::White;
-
-    for(int i=0; i<WINDOW_HEIGHT; ++i)
-    {
-        for(int j=0; j<WINDOW_WIDTH; ++j)
-        {
-            //mapping values
-            coord_real = slope_a * j + (-2);
-            coord_imaginary = slope_b * i + (-2);
-
-            prev_real = coord_real;
-            prev_imaginary = coord_imaginary;
-
-            for(counter = 0; counter<max_iterations; ++counter)
-            {
-                next_real = prev_real*prev_real - prev_imaginary*prev_imaginary; 
-                next_imaginary = 2*prev_real*prev_imaginary;
-
-                prev_real = next_real + coord_real;
-                prev_imaginary = next_imaginary + coord_imaginary;
-
-                if((prev_real + prev_imaginary) >= 2)
-                {
-                    break;
-                }
-            }
-
-            points_arr[i*WINDOW_WIDTH + j].position = sf::Vector2f(j, i);
-            if(counter == max_iterations)
-            {
-                points_arr[i*WINDOW_WIDTH + j].color = color1;
-            }
-            else
-            {
-                points_arr[i*WINDOW_WIDTH + j].color = color2;
-            }
-
-        }
-    }
-
-}
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Mandelbrot Set");
     window.setFramerateLimit(10);
 
-    sf::VertexArray points_arr(sf::Points, WINDOW_WIDTH * WINDOW_HEIGHT);
 
-    Mandelbrot(points_arr);
+    MandelBrot mandelbrot(window.getSize().x, window.getSize().y);
+    mandelbrot.go();
 
     while(window.isOpen())
     {
@@ -95,7 +27,44 @@ int main()
                 }
                 case sf::Event::KeyPressed:
                 {
-                    window.close();
+                    if(event.key.code == sf::Keyboard::Space)
+                    {
+                        window.close();
+                    }
+                    else if(event.key.code == sf::Keyboard::Z)
+                    {
+                        mandelbrot.control("zoomIn");
+                        mandelbrot.go();
+                    }
+                    else if(event.key.code == sf::Keyboard::X)
+                    {
+                        mandelbrot.control("zoomOut");
+                        mandelbrot.go();
+                    }
+                    else if(event.key.code == sf::Keyboard::Left
+                            || event.key.code == sf::Keyboard::H)
+                    {
+                        mandelbrot.control("moveLeft");
+                        mandelbrot.go();
+                    }
+                    else if(event.key.code == sf::Keyboard::Right
+                            || event.key.code == sf::Keyboard::L)
+                    {
+                        mandelbrot.control("moveRight");
+                        mandelbrot.go();
+                    }
+                    else if(event.key.code == sf::Keyboard::Up
+                            || event.key.code == sf::Keyboard::K)
+                    {
+                        mandelbrot.control("moveUp");
+                        mandelbrot.go();
+                    }
+                    else if(event.key.code == sf::Keyboard::Down
+                            || event.key.code == sf::Keyboard::J)
+                    {
+                        mandelbrot.control("moveDown");
+                        mandelbrot.go();
+                    }
                     break;
                 }
                 default:
@@ -106,10 +75,15 @@ int main()
         }
 
         window.clear();
-        window.draw(points_arr);
+        window.draw(mandelbrot.getArray());
         window.display();
     }
 
     return 0;
 }
 
+/*
+  mozna bybylo smart pointery tu wsadzic
+ */
+ 
+ 
