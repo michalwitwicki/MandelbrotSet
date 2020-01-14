453fbb02a0d30ad66361e3bf3fbbf7da6782c55e
Batawi <michal.witwicki14@gmail.com>
2020-01-14 21:51:18 +0100

    WIP: cmake and new structure
    Signed-off-by: Batawi <michal.witwicki14@gmail.com>

---
+++ build/CMakeFiles/myproject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myproject.dir/src/main.cpp.o"
+  "CMakeFiles/myproject.dir/src/mandelbrot.cpp.o"
+  "CMakeFiles/myproject.dir/src/stopwatch.cpp.o"
+  "myproject"
+  "myproject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
